fad92063092bfc3fcda9c1bddeec52f5dd4d806b
sklee <sklee@iisue.com>
2024-11-04 13:11:37 +0900

    Cache Xcode version to improve performance in getXcodeVersion function

--- src/xcode.ts
+++ src/xcode.ts
@@ -1,7 +1,12 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 
+let _xcodeVersion: number | undefined
 export async function getXcodeVersion(): Promise<number> {
+  if (_xcodeVersion !== undefined) {
+    return _xcodeVersion
+  }
+
   let output = ''
   const options = {
     listeners: {
@@ -14,7 +19,8 @@ export async function getXcodeVersion(): Promise<number> {
   await exec.exec('xcodebuild', ['-version'], options)
   const match = output.match(/Xcode (\d+)/)
   if (match) {
-    return parseInt(match[1], 10)
+    _xcodeVersion = parseInt(match[1], 10)
+    return _xcodeVersion
   } else {
     core.warning(
       `Failed to determine Xcode version from command 'xcodebuild -version'.`
